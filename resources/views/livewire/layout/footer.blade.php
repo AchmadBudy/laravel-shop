19bc894eb9ee2dbe2ed25bc026ab69f5e4955efc
AchmadBudy <nurachmadbudykurniawan@gmail.com>
2025-03-13 02:18:48 +0700

    fix (footer) : memperbaiki list layanan di footer

--- resources/views/livewire/layout/footer.blade.php
+++ resources/views/livewire/layout/footer.blade.php
@@ -5,22 +5,13 @@
                 <h5>Tentang Kami</h5>
                 <p>Toko online terpercaya sejak 2024</p>
             </div>
-            <div class="col-md-3">
+            <div class="col-md-6">
                 <h5>Layanan</h5>
                 <ul class="list-unstyled">
-                    <li><a href="#" class="text-white">Garansi</a></li>
+                    <li><a href="{{ route('garansi') }}" wire:navigate class="text-white">Garansi</a></li>
                     <li>
-                        <a href="#" class="text-white">Pengembalian</a>
+                        <a href="{{ route('contact-us') }}" wire:navigate class="text-white">Contact Us</a>
                     </li>
-                </ul>
-            </div>
-            <div class="col-md-3">
-                <h5>Kontak</h5>
-                <ul class="list-unstyled">
-                    <li>
-                        <i class="bi bi-envelope"></i> email@example.com
-                    </li>
-                    <li><i class="bi bi-phone"></i> 0812-3456-7890</li>
                 </ul>
             </div>
         </div>
