f384da4b99aac88f05b5fbb61ab33cc64d73c6e0
AchmadBudy <nurachmadbudykurniawan@gmail.com>
2025-06-16 00:30:03 +0700

    feat: add flash message handling with SweetAlert2 in AppLayout

--- resources/js/layouts/AppLayout.vue
+++ resources/js/layouts/AppLayout.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import AppLayout from '@/layouts/app/AppSidebarLayout.vue';
 import type { BreadcrumbItemType } from '@/types';
+import { usePage } from '@inertiajs/vue3';
+import { onUnmounted, watch } from 'vue';
+import Swal from 'sweetalert2';
 
 interface Props {
     breadcrumbs?: BreadcrumbItemType[];
@@ -9,6 +12,31 @@ interface Props {
 withDefaults(defineProps<Props>(), {
     breadcrumbs: () => [],
 });
+
+
+interface flashMessage {
+    success?: null;
+    error?: null;
+}
+
+onUnmounted(() => {
+    watch(() => usePage<{ flashMessage: flashMessage }>().props.flashMessage, (flashMessage: flashMessage) => {
+        if (flashMessage.success) {
+            Swal.fire({
+                icon: 'success',
+                title: 'Success',
+                text: flashMessage.success,
+            });
+        }
+        if (flashMessage.error) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: flashMessage.error,
+            });
+        }
+    }, { immediate: true });
+})
 </script>
 
 <template>
